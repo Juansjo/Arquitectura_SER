e804afa60e2f9e57fb3d919745aa3ba62d878fe8
Juan Jose Perez Gomez <jjperezg@ufpso.edu.co>
2026-05-07 22:49:11 -0500

    KAN-23 Crear coleccion session en Firestore

---
+++ src/types/index.ts
@@ -0,0 +1,51 @@
+import type { User } from 'firebase/auth';
+
+export interface FirebaseError {
+  code: string;
+  message: string;
+}
+
+export interface ModalData {
+  title: string;
+  message: string;
+  instructions?: string;
+  userData?: {
+    nombre?: string;
+    email?: string;
+    uid?: string;
+    provider?: string;
+  };
+}
+
+export interface FormData {
+  name?: string;
+  email: string;
+  password?: string;
+  confirmPassword?: string;
+}
+
+export interface UserData {
+  uid: string;
+  email: string | null;
+  displayName: string | null;
+  photoURL: string | null;
+  lastLogin: Date;
+  provider?: string;
+  name?: string;
+  role?: string;
+  createdAt?: Date;
+}
+
+
+export interface SessionLog {
+  id?: string;
+  userId: string;
+  userName: string | null;
+  userEmail: string | null;
+  loginTime: Date;
+  logoutTime?: Date | null;
+  sessionDuration?: number;
+  userPhotoURL?: string | null;
+}
+
+
